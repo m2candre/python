83c6f8424b288fc9008f73153cecc79c721e57b6
Marcos André <m2c.andre@gmail.com>
2019-07-08 21:33:49 -0300

    aula10

---
+++ aula10/01_teste_assert.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+#assert 2 == 2, 'Não'
+assert 2 != 2, 'Não'
---
+++ aula10/teste.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def soma(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+print(__name__)
+
+if __name__ == '__main__':
+    print(soma(5, 3))
+    print(sub(4, 2))
